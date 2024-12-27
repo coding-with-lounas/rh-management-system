3d2a3166c8557efdb924850bb9ffba879044ed98
lounas <lounasidjr@gmail.com>
2024-12-27 14:57:22 +0100

    sexe employe

--- rh_management/admin_rh/models.py
+++ rh_management/admin_rh/models.py
@@ -12,19 +12,24 @@ class Service(models.Model):
 class Employe(models.Model):
     nom = models.CharField(max_length=50)
     prenom = models.CharField(max_length=50)
-    sexe=models.CharField(max_length=6) 
+    sexe = models.CharField(max_length=10,choices=[('M', 'HOMME'), ('F', 'FEMME')],default='M')
     adresse=models.CharField(max_length=250)
     date_naissance = models.DateField()
     email = models.EmailField(max_length=100)
     date_embauche = models.DateField()
-    salaire_jour = models.DecimalField(max_digits=10, decimal_places=2)
     service = models.ForeignKey(Service, on_delete=models.CASCADE,related_name="rel_emp")
-
+    # salaire = models.ForeignKey(Salaire, on_delete=models.CASCADE)
     def __str__(self):
         return f'{self.prenom} {self.nom}'
+    
+# class Salaire(models.Model):
+
+#     salaire_jour = models.DecimalField(max_digits=10, decimal_places=2)
+#     salaire_mois = models.DecimalField(max_digits=10, decimal_places=2)
+#     salaire_annual = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Massrouf(models.Model):
-    employe = models.ForeignKey('Employe', on_delete=models.CASCADE)
+    employe = models.ForeignKey(Employe, on_delete=models.CASCADE)
     prix_avance=models.DecimalField(max_digits=10, decimal_places=2)
     date_demande = models.DateField(auto_now_add=True)
     justification = models.TextField()
