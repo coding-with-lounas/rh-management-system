c8e3fb8cfe81c53d5311d91c06dce762ce25914c
lounas <lounasidjr@gmail.com>
2024-12-30 21:19:01 +0100

    petit modification

--- rh_management/candidat/forms.py
+++ rh_management/candidat/forms.py
@@ -1,4 +1,3 @@
-# forms.py
 from django import forms
 from .models import Candidat
 
@@ -6,10 +5,4 @@ class CandidatForm(forms.ModelForm):
     class Meta:
         model = Candidat
         fields = ['nom', 'prénom', 'email', 'cv', 'recrutement']
-        widgets = {
-            'nom': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nom'}),
-            'prénom': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Prénom'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
-            'cv': forms.ClearableFileInput(attrs={'class': 'form-control'}),
-            'recrutement': forms.Select(attrs={'class': 'form-control'}),
-        }
+ 
--- rh_management/candidat/views.py
+++ rh_management/candidat/views.py
@@ -2,19 +2,6 @@ from django.shortcuts import render, redirect
 from .forms import CandidatForm
 from admin_rh.models import Recrutement
 
-# def candidat_create_view(request):
-#     if request.method == 'POST':
-#         form = CandidatForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('candidat_success')  # Redirige après succès
-#     else:
-#         form = CandidatForm()
-#     return render(request, 'candidat_form.html', {'form': form})
-
-# def candidat_success_view(request):
-#     return render(request, 'candidat_success.html')
-
 
 def candidat_create_view(request):
     recrutements = Recrutement.objects.all()
